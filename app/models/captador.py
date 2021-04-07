59b178153bf96f74a6c22cb2cfb5223067c5644d
pedroferronato <pedroluisfbar@gmail.com>
2021-04-07 14:07:49 -0400

    Desenvolvimento dos models - captador, doador, doação e hemocentro

---
+++ app/models/captador.py
@@ -0,0 +1,33 @@
+from app import db
+from flask_login import UserMixin
+
+from app.models.hemocentro import Hemocentro
+
+
+class Captador(db.Model, UserMixin):
+    __tablename__ = 'captadores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    hemocentro_id = db.Column(db.Integer, db.ForeignKey('hemocentros.id'), nullable=False)
+    nome = db.Column(db.String(200), nullable=False)
+    login = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    senha = db.Column(db.String(200), nullable=False)
+    administrador = db.Column(db.Boolean(), default=False)
+    servidor = db.Column(db.Boolean(), default=False)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __repr__(self):
+        return f'Captador: {self.email}'
+
--- app/models/doador.py
+++ app/models/doador.py
@@ -1,9 +1,72 @@
 from app import db
+from app.models.hemocentro import Hemocentro
+
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
 
 
 class Doador(db.Model):
-    __tablename__ = 'doador'
+    __tablename__ = 'doadores'
 
-    id = db.Column(db.Integer, primary_key=True)
+    numero_registro = db.Column(db.Integer, primary_key=True)
+    hemocentro_id = db.Column(db.Integer, db.ForeignKey('hemocentros.id'), primary_key=True)
     nome = db.Column(db.String(200), nullable=False)
+    cpf = db.Column(db.String(200), nullable=False)
+    data_de_nascimento = db.Column(db.Date(), nullable=False)
+    idade = db.Column(db.Integer)
+    tipo_sanguineo = db.Column(db.String(200), nullable=False)
+    municipio = db.Column(db.String(200), nullable=False)
+    telefone = db.Column(db.String(200), nullable=False)
+    celular = db.Column(db.String(200))
+    email = db.Column(db.String(200))
+    cadastro_SUS = db.Column(db.String(200), nullable=False)
+    sexo = db.Column(db.String(200), nullable=False)
+    estado_civil = db.Column(db.String(200))
+    avisado = db.Column(db.Boolean())
+    contatado = db.Column(db.Boolean())
+    contatos_preferidos = db.Column(db.String(200))
+    nome_mae = db.Column(db.String(200))
+    nome_pai = db.Column(db.String(200))
+    profissao = db.Column(db.String(200))
+    local_trabalho = db.Column(db.String(200))
+    fidelidade = db.Column(db.String(200))
+    inaptidao = db.Column(db.Boolean(), default=False)
+    final_inaptidao = db.Column(db.Date())
+    legado = db.Column(db.Boolean(), default=False)
+
+
+    def __repr__(self):
+        return f'Doador: {self.nome}'
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        LIMITE_SUPERIOR_IDADE_DOADOR = 60
+        hoje = date.today()
+        self.idade = (hoje.year - self.data_de_nascimento.year - ((hoje.month, hoje.day) < (self.data_de_nascimento.month, self.data_de_nascimento.day)))
+        if self.idade > LIMITE_SUPERIOR_IDADE_DOADOR:
+            self.legado = True
+
+
+    def definir_inaptidao(self, inaptidao, data):
+        self.inaptidao = inaptidao
+        self.final_inaptidao = data
+        if data is None and inaptidao:
+            self.final_inaptidao = datetime.today() + relativedelta(months=1)
+            
+    
+    def get_idade(self):
+        self.idade = (hoje.year - self.data_de_nascimento.year - ((hoje.month, hoje.day) < (self.data_de_nascimento.month, self.data_de_nascimento.day)))
+        db.session.add(self)
+        db.session.commit()
+        return self.idade
+
+
+    def dia_do_aniversario(self):
+        # Deve retornar um boleano (é ou não dia de seu aniversário)
+        pass
+
+
+    def data_ultima_doacao(self):
+        pass
 
