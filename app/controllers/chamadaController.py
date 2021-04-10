a6293c0f56efc503d3af3eabb0e070fbd1c788ab
BrunoDelani <br.delani@gmail.com>
2021-04-10 16:25:59 -0400

    Finalizada as páginas de convocação
    - Arquivos Criados/Modificados :
    - Base Convocação - HTML
    - Convocação Tipada (ALTERADO) - HTML
    - Convocação Emergencial - HTML
    - Convocação Localidades externas - HTML
    - Convocação convocados - HTML
    - Convocação controller (ALTERADO) - JS
    - Base conteúdo (Alterado) - CSS
    - Base convocação - CSS
    - Convocação Tipada (DELETADO) - CSS

--- app/controllers/chamadaController.py
+++ app/controllers/chamadaController.py
@@ -3,5 +3,40 @@ from flask import render_template
 
 @flaskApp.route('/convocacao-tipada')
 def chamada_tipada():
-    return render_template("convocacaoTipada.html")
+    return render_template("convocacaoTipada.html", tipo="tipada")
+
+
+@flaskApp.route('/convocacao-tipada/resultado')
+def chamada_tipada_resultado():
+    return render_template("convocacaoTipada.html", tipo="tipada", resultado=True)
+
+
+@flaskApp.route('/convocacao-emergencial')
+def chamada_emergencial():
+    return render_template("convocacaoEmergencial.html", tipo="emergencial")
+
+
+@flaskApp.route('/convocacao-emergencial/resultado')
+def chamada_emergencial_resultado():
+    return render_template("convocacaoEmergencial.html", tipo="emergencial", resultado=True)
+
+
+@flaskApp.route('/convocacao-localidades-externas')
+def chamada_localidades_externas():
+    return render_template("convocacaoLocalidadesExternas.html", tipo="localidades-externas")
+
+
+@flaskApp.route('/convocacao-localidades-externas/resultado')
+def chamada_localidades_externas_resultado():
+    return render_template("convocacaoLocalidadesExternas.html", tipo="localidades-externas", resultado=True)
+
+
+@flaskApp.route('/convocacao-convocados')
+def chamada_convocados():
+    return render_template("convocacaoConvocados.html", tipo="convocados")
+
+
+@flaskApp.route('/convocacao-convocados/resultado')
+def chamada_convocados_resultado():
+    return render_template("convocacaoConvocados.html", tipo="convocados", resultado=True)
 
