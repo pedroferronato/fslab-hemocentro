090d321edece880fcccea81a3e5e73cb5f4f430c
João Vitor Lopes <jvlaaa@gmail.com>
2021-04-10 13:24:19 -0400

    criando a pagina de captador
    Realizado a criação da pagina de cadastrar um novo captador

--- app/controllers/captadorController.py
+++ app/controllers/captadorController.py
@@ -4,3 +4,8 @@ from flask import render_template
 @login_manager.user_loader
 def get_user(id):
     return Captador.query.filter_by(id=id).first()
+
+
+@flaskApp.route('/novo-captador')
+def novo_captador():
+    return render_template("captador.html")
