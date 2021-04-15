a3a09834c9b256840f29aec7fc998647caebe09c
BrunoDelani <br.delani@gmail.com>
2021-04-15 18:10:43 -0400

    Inserir-captador-no-banco
    Inserir captador
    Validação dos campos
    Validação de senhas iguais
    Mascara para telefone

--- app/controllers/captadorController.py
+++ app/controllers/captadorController.py
@@ -1,14 +1,59 @@
-from app import flaskApp, login_manager
-from flask import render_template
+from app import flaskApp, login_manager, db
+from flask import render_template, redirect, request, url_for
+from app.models.hemocentro import Hemocentro
+from app.models.captador import Captador
+
 
 @login_manager.user_loader
 def get_user(id):
     return Captador.query.filter_by(id=id).first()
 
 
-@flaskApp.route('/novo-captador')
+@flaskApp.route('/captador', methods=['GET', 'POST'])
 def novo_captador():
-    return render_template("captador.html")
+    sucesso = request.args.get('sucesso')
+    senhasDiferentes = request.args.get('senhas')
+
+    hemocentros = Hemocentro.query.all()
+    if request.method == 'GET':
+        return render_template("captador.html", sucesso=sucesso, hemocentros=hemocentros)
+    elif request.method == 'POST':
+        continuar = False
+        if request.form['inserir'] == 'Inserir e continuar':
+            continuar = True
+        
+        nome = request.form['nome']
+        celular = request.form['celular']
+        hemocentro = request.form['hemocentro']
+        adm = request.form['adm']
+        mail = request.form['mail']
+        login = request.form['login']
+        senha = request.form['senha']
+
+        #TODO: CRIPTOGRAFAR SENHA
+
+        try:
+            if adm == 'Captador':
+                cap = Captador(nome=nome, celular=celular, hemocentro_id=hemocentro, email=mail, login=login, senha=senha, administrador=False, servidor=False)
+            elif adm == 'Administrador':
+                cap = Captador(nome=nome, celular=celular, hemocentro_id=hemocentro, email=mail, login=login, senha=senha, administrador=True, servidor=False)
+            elif adm == 'Servidor':
+                cap = Captador(nome=nome, celular=celular, hemocentro_id=hemocentro, email=mail, login=login, senha=senha, administrador=True, servidor=True)
+            else:
+                pass
+            db.session.add(cap)
+            db.session.commit()
+        except:
+            print("An exception occurred")
+            return render_template("paginaInicial.html", sucesso="") # TODO Gerar uma página de erro
+
+        if continuar:
+            return redirect(url_for("novo_captador", sucesso=True))
+        else:
+            return redirect(url_for('inicial', sucesso="sucesso"))
+        
+        
+
 
 
 @flaskApp.route('/alterar-captador')
@@ -16,7 +61,7 @@ def alterar_captador():
     return render_template("captador.html", alterar=True)
 
 
-@flaskApp.route('/consultar-captadores') 
+@flaskApp.route('/consultar/captador') 
 def consultar_captador():
     return render_template("consultaCaptador.html")
 
---
+++ app/static/js/validarCaptador.js
@@ -0,0 +1,139 @@
+$(".alert").delay(2000).slideUp(200, function() {
+    $(this).alert('close')
+})
+
+function validarCampos(evento) {
+    var nome = document.getElementById("nome")
+    var lbNome = document.getElementById("lbNome")
+    var celular = document.getElementById("celular")
+    var lbCelular = document.getElementById("lbCelular")
+    var hemocentro = document.getElementById("hemocentro")
+    var lbHemocentro = document.getElementById("lbHemocentro")
+    var mail = document.getElementById("mail")
+    var lbMail = document.getElementById("lbMail")
+    var login = document.getElementById("login")
+    var lbLogin = document.getElementById("lbLogin")
+    var senha = document.getElementById("senha")
+    var lbSenha = document.getElementById("lbSenha")
+    var confirmarSenha = document.getElementById("confirmarSenha")
+    var lbConfirmarSenha = document.getElementById("lbConfirmarSenha")
+
+    var alerta = false
+
+    if (nome.value == undefined || nome.value == null || nome.value == "" || nome.value == " ") {
+        if (!nome.classList.contains('borda-alerta')) {
+            nome.classList.add('borda-alerta')
+            lbNome.classList.add('txt-alerta')
+        }
+        alerta = true
+    }
+
+    if (celular.value == undefined || celular.value == null || celular.value == "" || celular.value == " ") {
+        if (!celular.classList.contains('borda-alerta')) {
+            celular.classList.add('borda-alerta')
+            lbCelular.classList.add('txt-alerta')
+        }
+        alerta = true
+    }
+
+    if (hemocentro.value == "Selecione") {
+        if (!hemocentro.classList.contains('borda-alerta')) {
+            hemocentro.classList.add('borda-alerta')
+            lbHemocentro.classList.add('txt-alerta')
+        }
+        alerta = true
+    }
+
+    if (mail.value == undefined || mail.value == null || mail.value == "" || mail.value == " ") {
+        if (!mail.classList.contains('borda-alerta')) {
+            mail.classList.add('borda-alerta')
+            lbMail.classList.add('txt-alerta')
+        }
+        alerta = true
+    }
+
+    if (login.value == undefined || login.value == null || login.value == "" || login.value == " ") {
+        if (!login.classList.contains('borda-alerta')) {
+            login.classList.add('borda-alerta')
+            lbLogin.classList.add('txt-alerta')
+        }
+        alerta = true
+    }
+    
+    if (senha.value == undefined || senha.value == null || senha.value == "" || senha.value == " ") {
+        if (!senha.classList.contains('borda-alerta')) {
+            senha.classList.add('borda-alerta')
+            lbSenha.classList.add('txt-alerta')
+        }
+        alerta = true
+    }
+    
+    if (confirmarSenha.value == undefined || confirmarSenha.value == null || confirmarSenha.value == "" || confirmarSenha.value == " ") {
+        if (!confirmarSenha.classList.contains('borda-alerta')) {
+            confirmarSenha.classList.add('borda-alerta')
+            lbConfirmarSenha.classList.add('txt-alerta')
+        }
+        alerta = true
+    }
+
+    if (senha.value != confirmarSenha.value) {
+        if (!senha.classList.contains('borda-alerta')) {
+            senha.classList.add('borda-alerta')
+            lbSenha.classList.add('txt-alerta')
+        }
+        if (!confirmarSenha.classList.contains('borda-alerta')) {
+            confirmarSenha.classList.add('borda-alerta')
+            lbConfirmarSenha.classList.add('txt-alerta')
+        }
+        alerta = true
+    }
+
+    if (alerta) evento.preventDefault()
+}
+
+function removerClasseAlerta(elemento){
+    if (nome.classList.contains('borda-alerta') && elemento == 'nome') {
+        nome.classList.remove('borda-alerta')
+        lbNome.classList.remove('txt-alerta')
+    }
+
+    if (celular.classList.contains('borda-alerta') && elemento == 'celular') {
+        celular.classList.remove('borda-alerta')
+        lbCelular.classList.remove('txt-alerta')
+    }
+
+    if (hemocentro.classList.contains('borda-alerta') && elemento == 'hemocentro') {
+        hemocentro.classList.remove('borda-alerta')
+        lbHemocentro.classList.remove('txt-alerta')
+    }
+
+    if (mail.classList.contains('borda-alerta') && elemento == 'mail') {
+        mail.classList.remove('borda-alerta')
+        lbMail.classList.remove('txt-alerta')
+    }
+
+    if (login.classList.contains('borda-alerta') && elemento == 'login') {
+        login.classList.remove('borda-alerta')
+        lbLogin.classList.remove('txt-alerta')
+    }
+
+    if (senha.classList.contains('borda-alerta') && elemento == 'senha') {
+        senha.classList.remove('borda-alerta')
+        lbSenha.classList.remove('txt-alerta')
+    }
+
+    if (confirmarSenha.classList.contains('borda-alerta') && elemento == 'confirmarSenha') {
+        confirmarSenha.classList.remove('borda-alerta')
+        lbConfirmarSenha.classList.remove('txt-alerta')
+    }
+}
+
+function limpar() {
+    nome.value = ""
+    celular.value = ""
+    hemocentro.value = "Selecione"
+    mail.value = ""
+    login.value = ""
+    senha.value = ""
+    confirmarSenha.value = ""
+}
