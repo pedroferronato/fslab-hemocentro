ff3a0faabec24657e4cf247f9971532fd6712f15
BrunoDelani <54160702+BrunoDelani@users.noreply.github.com>
2021-04-26 18:38:09 -0400

    Update mascaras.js

--- app/static/js/mascaras.js
+++ app/static/js/mascaras.js
@@ -5,5 +5,3 @@ $('#celular').mask('(00) 0 0000-0000', {'translation': {0: {pattern: /[0-9*]/}}}
 $('.data').mask('00/00/0000', {'translation': {0: {pattern: /[0-9*]/}}});
 
 $('#cpf').mask('000.000.000-00', {'translation': {0: {pattern: /[0-9*]/}}});
-
-$('.cpf').mask('000.000.000-00', {'translation': {0: {pattern: /[0-9*]/}}});
