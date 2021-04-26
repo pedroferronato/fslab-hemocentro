107e9f5ef45ae9ae60b38c4c6176da75165b95ac
BrunoDelani <54160702+BrunoDelani@users.noreply.github.com>
2021-04-26 18:39:31 -0400

    Delete mascaras.js

--- app/static/js/mascaras.js
+++
@@ -1,7 +0,0 @@
-$('#telefone').mask('(00) 0000-0000', {'translation': {0: {pattern: /[0-9*]/}}});
-
-$('#celular').mask('(00) 0 0000-0000', {'translation': {0: {pattern: /[0-9*]/}}});
-
-$('.data').mask('00/00/0000', {'translation': {0: {pattern: /[0-9*]/}}});
-
-$('#cpf').mask('000.000.000-00', {'translation': {0: {pattern: /[0-9*]/}}});
