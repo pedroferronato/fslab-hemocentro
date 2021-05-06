6a3b1b531ea8d73fb16175c84064fea16edd29e8
pedroferronato <pedroluisfbar@gmail.com>
2021-05-06 08:05:29 -0400

    Adição da foncionalidade de copy to clipboard e aprimoramento dos campos município

---
+++ app/static/js/copyToClipboard.js
@@ -0,0 +1,9 @@
+function copyElementText(id) {
+    var text = document.getElementById(id).innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+}
