b6e51859bedd567d3e765919b94336b7d95a435c
pedroferronato <pedroluisfbar@gmail.com>
2021-05-11 11:33:57 -0400

    Tela de convocação de doadores de localidades externas

---
+++ app/static/js/convocacaoExterna.js
@@ -0,0 +1,54 @@
+$(".alert").delay(2000).slideUp(200, function() {
+    $(this).alert('close')
+})
+
+var tipo = document.getElementById("tipagem")
+var lbTipagem = document.getElementById("lbTipagem")
+var municipio = document.getElementById("municipio")
+var lbMunicipio = document.getElementById("lbMunicipio")
+
+
+function validar(evento) {
+    if (tipo.value == "null") {
+        if (!tipo.classList.contains('borda-alerta')) {
+            tipo.classList.add('borda-alerta')
+            lbTipagem.classList.add('txt-alerta')
+        }
+        evento.preventDefault()
+    }
+
+    if (municipio.value == "null") {
+        if (!municipio.classList.contains('borda-alerta')) {
+            municipio.classList.add('borda-alerta')
+            lbMunicipio.classList.add('txt-alerta')
+        }
+        evento.preventDefault()
+    }
+}
+
+function remover(elemento) {
+    if (tipo.classList.contains('borda-alerta') && elemento == 'tipo') {
+        tipo.classList.remove('borda-alerta')
+        lbTipagem.classList.remove('txt-alerta')
+    }
+
+    if (municipio.classList.contains('borda-alerta') && elemento == 'municipio') {
+        municipio.classList.remove('borda-alerta')
+        lbMunicipio.classList.remove('txt-alerta')
+    }
+}
+
+function adicionarPagina(valor) {
+    document.getElementById("page").value = valor
+}
+
+function adicionarTelefonado(valor, evento) {
+    let telefonados = document.getElementById("telefonados");
+    if (!evento.target.classList.contains("azul")) {
+        telefonados.value += valor + "&";
+        evento.target.classList.add("azul");
+    } else {
+        telefonados.value = telefonados.value.replace(valor + "&", '');
+        evento.target.classList.remove("azul");
+    }
+}
