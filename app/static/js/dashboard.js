34b03486b58de31cbea005b8f1d9f78adbe70248
BrunoDelani <br.delani@gmail.com>
2021-06-04 18:25:00 -0400

    dashboard-atualizado
    HTML dashboard atualizado e responsivo.

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,12 +1,3 @@
-// const dashboard =  fetch("http://127.0.0.1:5000/dashboard/carregamento")
-// .then((response) => response.json()).then((data) => {
-//     return data
-// });
-
-// const getData = async () => {
-//     const informacao = await dashboard;
-//     return informacao
-// };
 
 function getData() {
     let valores = document.getElementsByClassName("valor")
@@ -46,3 +37,43 @@ var chartGraph = new Chart(ctx, {
         }
     },
 });
+
+var pizza = document.getElementById("pizza");
+
+var chartPizza = new Chart(pizza, {
+    type: 'doughnut',
+    data: {
+        labels: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+        datasets: [
+            {
+                label: "DOAÇÕES 2021",
+                data: getData(),
+                backgroundColor: [
+                 '#71F77F',
+                 '#13751D',
+                 '#9195F7',
+                 '#222575',
+                 '#F77D76',
+                 '#751A15',
+                 '#F7EA7B',
+                 '#756C17',
+                ]
+            },
+        ]
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: "Doações por tipo",
+                font: {
+                    size: 24,
+                }
+            },
+            legend: {
+                display: true,
+            }
+        }
+    },
+});
+
